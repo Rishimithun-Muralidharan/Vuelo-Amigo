51a75e2fad06721bac43937a072bc0b05ffbe4bd
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-03-19 12:02:24 +0530

    backend complete. Frontend initiated.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 /*REGISTER USER*/
-export const rgister = async (req, res) => {
+export const register = async (req, res) => {
 	try {
 		const {
 			firstName,
