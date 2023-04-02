7bbf1de5312c5af0117e2867966a5cd5b7666f17
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-04-03 02:18:08 +0530

    set up database models using SQLAlchemy

--- backend-final-sdgp/main.py
+++ backend-final-sdgp/main.py
@@ -2,6 +2,15 @@ from website import create_app
 
 app = create_app()
 
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
 if __name__ == '__main__':
+    # Quick test configuration. Please use proper Flask configuration options
+    # in production settings, and use a separate file or environment variables
+    # to manage the secret key!
+    app.secret_key = 'super secret key'
+    #app.config['SESSION_TYPE'] = 'filesystem'
+
     app.run(debug=True)
     
--- backend-final-sdgp/website/auth.py
+++ backend-final-sdgp/website/auth.py
@@ -1,16 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    
     return render_template("login.html" , user="" )
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.' , category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters.' , category='error')
+            
+        elif password1 != password2:
+            flash('Passwords do not match.' , category='error')
+            
+        elif len(password1) < 7:
+            flash('Password must be at least 6 characters.' , category='error')
+        else:
+            # add user to database
+            flash('Account created!' , category='success')
+
     return render_template("sign_up.html", user="")
 
