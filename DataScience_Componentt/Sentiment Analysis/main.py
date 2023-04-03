fcdbb0ad1bb355b55ad544beb877c6832b03d84f
Sachintha Sasanjaya <sachinthasasanjayaliyanage2001@gmail.com>
2023-04-03 21:04:52 +0530

    Tokenization and Stop Words (NLP)

--- DataScience_Componentt/Sentiment Analysis/main.py
+++ DataScience_Componentt/Sentiment Analysis/main.py
@@ -1,10 +1,25 @@
-#Cleaning Text
-#1) Create Text file and take text from it
-#2) Convert the letters into lowercase ('Apple' is not equal to 'apple')
-#3) Removing punctuations like.,!? etc. (Hi! This is buildwithpython.)
-
 import string
-text  = open("read.txt" , encoding= "utf-8").read()
+text = open("read.txt", encoding="utf-8").read()
 lower_case = text.lower()
-cleaned_text = lower_case.translate(str.maketrans('' , '' , string.punctuation))
-print(cleaned_text)
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+
+tokenized_words = cleaned_text.split()
+print (tokenized_words)
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+final_words = []
+for word in tokenized_words:
+    if word not in stop_words:
+        final_words.append(words)
+
+print(final_words)
