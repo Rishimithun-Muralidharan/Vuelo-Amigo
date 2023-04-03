fc29a255b962297b414c9ff0b0f5202842772403
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-04-03 08:13:12 +0530

    fixed flask errors

--- backend-final-sdgp/website/__init__.py
+++ backend-final-sdgp/website/__init__.py
@@ -1,12 +1,38 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+#db = SQLAlchemy()
+#DB_NAME = "database.db"
+
+# def create_app():
+#    app = Flask(__name__)
+#    app.config['SECRET_KEY'] = 'gjgygjgjyg ouyiutyry'
+#    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+#    db.init_app(app)
+
+#    from .views import views
+#    from .auth import auth
+
+#    app.register_blueprint(views, url_prefix='/')
+#    app.register_blueprint(auth, url_prefix='/')
+
+#    from .models import User, Note
+
+#    create_database(app)
+#    return app 
+
+# def create_database(app):
+#    if not path.exists('website/' + DB_NAME):
+#        db.create_all (app=app)
+#        print("Created Database!")
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET KEY'] = 'gjgygjgjyg ouyiutyry'
+    app.config['SECRET_KEY'] = 'gjgygjgjyg ouyiutyry'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -15,4 +41,10 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    return app 
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+
+    return app
