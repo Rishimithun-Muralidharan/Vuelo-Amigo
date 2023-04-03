aaccd99caf621e3d01b43408a20fda6dd4b50eec
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-04-03 11:26:18 +0530

    log in functionality implemented

--- backend-final-sdgp/website/auth.py
+++ backend-final-sdgp/website/auth.py
@@ -7,6 +7,20 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+            else: 
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')        
+
+
     
     return render_template("login.html" , user="" )
 
@@ -22,7 +36,12 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email already exists.' , category='error' )
+
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.' , category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.' , category='error')
