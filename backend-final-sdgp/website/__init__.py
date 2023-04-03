768251fee07cfdf7ddb04ea7d3f8ae28820d6bc4
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-04-03 13:01:00 +0530

    debugged flask's login module

--- backend-final-sdgp/website/__init__.py
+++ backend-final-sdgp/website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 #db = SQLAlchemy()
 #DB_NAME = "database.db"
@@ -36,6 +37,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+    
+
     from .views import views
     from .auth import auth
 
@@ -46,6 +49,15 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))    
+        
+        
 
     return app
 
--- backend-final-sdgp/website/auth.py
+++ backend-final-sdgp/website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash 
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,8 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else: 
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -25,8 +28,10 @@ def login():
     return render_template("login.html" , user="" )
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -56,8 +61,9 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!' , category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user="")
+    return render_template("sign_up.html", user = current_user)
 
