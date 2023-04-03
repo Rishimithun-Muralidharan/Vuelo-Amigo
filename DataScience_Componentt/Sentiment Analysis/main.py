d26c33420540f7c8d4df576e04cb900a21d9e87e
Sachintha Sasanjaya <sachinthasasanjayaliyanage2001@gmail.com>
2023-04-03 20:39:41 +0530

    Cleaning Text for Natural Language Processing (NLP)

--- DataScience_Componentt/Sentiment Analysis/main.py
+++ DataScience_Componentt/Sentiment Analysis/main.py
@@ -1,15 +1,10 @@
 #Cleaning Text
-#1)Create Text file and take text from it
-#2)Conver the letters into lowercase
-#3)Removing punctuations
+#1) Create Text file and take text from it
+#2) Convert the letters into lowercase ('Apple' is not equal to 'apple')
+#3) Removing punctuations like.,!? etc. (Hi! This is buildwithpython.)
 
 import string
 text  = open("read.txt" , encoding= "utf-8").read()
 lower_case = text.lower()
 cleaned_text = lower_case.translate(str.maketrans('' , '' , string.punctuation))
 print(cleaned_text)
-sachintha Sasanjaya
-Vinuja Rajapaksha
-jannnn
-
-
