0b183e26760efacc832225f459ace779d917d431
Akila-Gamage <akila.20210423@iit.ac.lk>
2023-04-05 21:35:46 +0530

    connecting the frontend and backend

--- backend-final-sdgp/website/auth.py
+++ backend-final-sdgp/website/auth.py
@@ -17,7 +17,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('views.dashboard'))
             else: 
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -25,7 +25,7 @@ def login():
 
 
     
-    return render_template("login.html" , user=current_user )
+    return render_template("signin.html" , user=current_user )
 
 @auth.route('/logout')
 @login_required
@@ -44,26 +44,26 @@ def sign_up():
         user = User.query.filter_by(email=email).first()
 
         if user:
-            flash('Email already exists.' , category='error' )
+            flash('Email already exists.', category='error')
 
         elif len(email) < 4:
-            flash('Email must be greater than 3 characters.' , category='error')
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
-            flash('First name must be greater than 1 characters.' , category='error')
+            flash('First name must be greater than 1 characters.', category='error')
             
         elif password1 != password2:
-            flash('Passwords do not match.' , category='error')
+            flash('Passwords do not match.', category='error')
             
         elif len(password1) < 7:
-            flash('Password must be at least 6 characters.' , category='error')
+            flash('Password must be at least 6 characters.', category='error')
         else:
             # add user to database
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Account created!' , category='success')
-            return redirect(url_for('views.home'))
+            flash('Account created!', category='success')
+            return redirect(url_for('views.dashboard'))
 
-    return render_template("sign_up.html", user = current_user)
+    return render_template("signup.html", user = current_user)
 
--- backend-final-sdgp/website/views.py
+++ backend-final-sdgp/website/views.py
@@ -5,10 +5,23 @@ from . import db
 import json
 views = Blueprint('views', __name__)
 
+
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    return render_template("index.html", user=current_user)
+@views.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template("dashboard.html", user=current_user)
+
+@views.route('/addreview', methods=['GET', 'POST'])
+@login_required
+def addreview():
     if request.method == 'POST':
+        # username = request.form.get('username')
+        # title = request.form.get('title')
+        # rating = request.form.get('rating')
         note = request.form.get('note')
 
         if len(note) < 1:
@@ -17,9 +30,13 @@ def home():
             new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
-            flash('Note added!', category='success')    
+            flash('Note added!', category='success')
+    return render_template("career.html", user=current_user)
 
-    return render_template("home.html", user=current_user)
+@views.route('/viewreview', methods=['GET', 'POST'])
+def viewreview():
+    return render_template("blog-1.html", user=current_user)
+
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
     note = json.loads(request.data)
@@ -31,5 +48,6 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
-              
+
+
 
