e506ecd284c8f2c701e4b3406562fa1c9c495873
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-04-06 04:03:47 +0530

    added streamlit component

---
+++ Sentiment Analysis-Real Time  - Streamlit Library/main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+st.write("#Real time Sentiment Analysis")
+
+user_input = st.text_input("Please rate our services >>:")
+nltk.download("vader_lexicon")
+s = SentimentIntensityAnalyzer()
+score = s.polarity_scores(user_input)
+
+if score == 0:
+    st.write(" ")
+elif score["neg"] !=0:
+    st.write("# Negative 😫 ")
+elif score["pos"] !=0:
+    st.write("# Positive 😀 ")
+
+
